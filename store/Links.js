7f262795f015f9196e0b59abd55417105d18436b
Pinky515 <borroumt@gmail.com>
2023-10-11 17:59:56 -0500

    Add icons for navigation

--- store/Links.js
+++ store/Links.js
@@ -6,3 +6,8 @@ export default [
   { title: "Schedule", text: "My Schedule" },
   { title: "MyGarden", text: "My Garden" }
 ];
+{/* <i class="fa-solid fa-house-chimney-window" style="color: #f7f7ffff;"></i>;
+<i class="fa-solid fa-seedling" style="color: #32e875ff;"></i>;
+<i class="fa-solid fa-clock" style="color: #d76a03ff;"></i>;
+<i class="fa-solid fa-book-open-reader" style="color: #a64253ff;"></i>;
+<i class="fa-regular fa-comments" style="color: #279af1ff;"></i> */}
